73c975cb7f9f8e8342965ce3f7164795ac8d6d40
aydencodes <84056225+aydencodes@users.noreply.github.com>
2021-08-16 18:49:42 -0700

    Add files via upload

---
+++ slingshot.js
@@ -0,0 +1,26 @@
+class SlingShot{
+    constructor(bodyA,pointB){
+        var options = {
+            bodyA:bodyA,
+            pointB:pointB,
+            stiffness:0.004,
+            length:10,
+        }
+         this.pointB = pointB
+        this.chain=Constraint.create(options)
+        World .add(world,this.chain)
+            }
+            fly(){
+                this.chain.bodyA = null;
+            }
+            attach(body){this.chain.bodyA =body}
+            display(){
+                if(this.chain.bodyA){
+                var pointA = this.chain.bodyA.position
+                var pointB = this.pointB
+                strokeWeight(4)
+                fill ("yellow")
+                line (pointA.x,pointA.y,pointB.x,pointB.y)
+            }
+        }
+        }
